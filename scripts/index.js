fd1b332f267fa8e4f459fd2f1b9b7fbd8e59f193
Kummirai <ajaxmilton@hotmail.com>
2025-02-15 13:11:10 +0200

    disable navbar

--- scripts/index.js
+++ scripts/index.js
@@ -44,3 +44,11 @@
             });
         });
 
+        // function showSection(sectionId) {
+        //     // Hide all sections
+        //     document.getElementById('overview').style.display = 'none';
+        //     document.getElementById('curriculum').style.display = 'none';
+
+        //     // Show the selected section
+        //     document.getElementById(sectionId).style.display = 'block';
+        // }
